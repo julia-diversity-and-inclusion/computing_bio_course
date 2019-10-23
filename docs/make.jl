01790a59c85c96168bfcc78aedcf02ed1756f358
Kevin Bonham <kevbonham@gmail.com>
2019-10-23 10:45:25 -0400

    try out docs build on PR

--- docs/make.jl
+++ docs/make.jl
@@ -17,9 +17,24 @@ makedocs(
         prettyurls = get(ENV, "CI", nothing) == "true")
 )
 
-deploydocs(
-    repo = "github.com/wellesley-bisc195/computing_bio_course.git",
-    target = "build",
-    deps = nothing,
-    make = nothing
-)
+const repo="github.com/wellesley-bisc195/computing_bio_course.git"
+const PR = get(ENV, "TRAVIS_PULL_REQUEST", "false")
+if PR == "false"
+    # Normal case, only deply docs if merging to master or release tagged
+    deploydocs(repo=repo)
+else
+    @info "Deploying review docs for PR #$PR"
+    # TODO: remove most of this once https://github.com/JuliaDocs/Documenter.jl/issues/1131 is resolved
+
+    # Overwrite Documenter's function for generating the versions.js file
+    foreach(Base.delete_method, methods(Documenter.Writers.HTMLWriter.generate_version_file))
+    Documenter.Writers.HTMLWriter.generate_version_file(_, _) = nothing
+    # Overwrite necessary environment variables to trick Documenter to deploy
+    ENV["TRAVIS_PULL_REQUEST"] = "false"
+    ENV["TRAVIS_BRANCH"] = "master"
+
+    deploydocs(
+        devurl="preview-PR$(PR)",
+        repo=repo,
+    )
+end
